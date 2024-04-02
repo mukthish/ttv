0bb021b775806f69974765b52a6188530cc91bfe
psentaur <mukthish.2000@gmail.com>
2024-04-03 00:58:09 +0530

    adding joi validation

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,25 @@
-import express from 'express'
+import express from 'express';
+import Joi from 'joi';
+import ExpressValidation from 'express-joi-validation';
 import { postLogin, postRegister } from '../controllers/controllers.js';
 
 const router = express.Router();
 
-router.post('/register', postRegister);
+const validator = ExpressValidation.createValidator({});
 
-router.post('/login', postLogin);
+const registerSchema = Joi.object({
+    username: Joi.string().min(3).max(12).required(),
+    password: Joi.string().min(3).max(12).required(),
+    email: Joi.string().email().required(),
+});
+
+const loginSchema = Joi.object({
+    password: Joi.string().min(3).max(12).required(),
+    email: Joi.string().email().required(),
+});
+
+router.post('/register', validator.body(registerSchema), postRegister);
+
+router.post('/login', validator.body(loginSchema), postLogin);
 
 export default router;
