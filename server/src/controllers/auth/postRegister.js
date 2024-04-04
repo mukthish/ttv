15f413f0e35c3ef3f54a466534c80ba2beb00f0e
psentaur <mukthish.2000@gmail.com>
2024-04-05 01:23:12 +0530

    adding login functionality

--- server/src/controllers/auth/postRegister.js
+++ server/src/controllers/auth/postRegister.js
@@ -1,3 +1,53 @@
+import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 export const postRegister = async (req, res) => {
-    return res.send('this is register route');
+    try {
+        const { username, email, password } = req.body;
+
+        const userExists = await User.exists({ email });
+
+        if (userExists) {
+            return res.status(409).send("E-mail already in use.");
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            username,
+            email :email.toLowerCase(),
+            password: encryptedPassword,
+        });
+
+        // Create JWT token
+        const token = jwt.sign(
+            // user details which we would like to encrypt in JWT token
+            {
+                userID: user._id,
+                email,
+            },
+            //secret
+            process.env.TOKEN_KEY,
+            // additional config
+            {
+                expiresIn: "8h",
+            }
+        );
+
+        //  send success response back to the user with data of registered user and JWT
+        return res.status(201).json({
+            userDetails: {
+                email,
+                username,
+                token,
+            },
+        })
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send('Error occored. Please try again');
+    }
+
+    return res.send("user has been added to the database");
 };
